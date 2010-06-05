4c7567540c2a95de464dfeecbe4d0aff197f10d8
jerome <jerome.etienne@gmail.com>
2010-06-05 10:07:46 +0200

    test.js: beautifullation 	modified:   test.js

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 var neoip	= require('./neoip');
 var sys		= require('sys');
 
-neoip.discover_app("oload", function(version, strerror){
+neoip.discover_app("casti", function(version, strerror){
+	// TODO what about the port ?
 	sys.puts("version:"+version);
 	sys.puts("strerror:"+strerror);
 });
