b9f9879050feb8ce058a8dd802824715e68c20d4
jerome <jerome.etienne@gmail.com>
2010-06-23 06:10:01 +0200

    renamed tmp/ into contrib/

--- tmp/oload_stress_test/prout.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-// on linux ubuntu 10.04, nodejs 0.1.97
-// without sys.puts('trace1'), this display trace2 without keyboard interaction
-// with    sys.puts('trace1'), this *needs* to press a keyboard key to print 
-var sys		= require('sys');
-//sys.puts("trace1");
-var http	= require('http');
-sys.puts("trace2");
