260b6d6a7ddd33e9c98a8ca9eace1352d49ff0e8
Jerome Etienne <jerome.etienne@gmail.com>
2010-07-08 08:44:18 +0200

    casti_stress_test: more work 	modified:   contrib/casti_stress_test/casti_ctrl_t.js 	modified:   contrib/casti_stress_test/main.js 	modified:   contrib/vendor/node-chargen 	modified:   lib/neoip_rpc.js

--- contrib/casti_stress_test/casti_ctrl_t.js
+++ contrib/casti_stress_test/casti_ctrl_t.js
@@ -1,29 +1,51 @@
 var assert	= require('assert');
-var rpc_call	= require('../../lib/neoip_rpc').rpc_call;
+var neoip_rpc	= require('../../lib/neoip_rpc');
 
 /**
  * - opts.refresh_period : period in msec
  * - opts.call_url: call url for neoip-casti
- * - opts.casti_opts: all the parameters for casti call url
+ * - opts.call_args: all the parameters for casti call url
 */
 var casti_ctrl_t	= function(opts, user_cb){
 	var self		= {};
 	var refresh_timerid	= null;
+	var rpc_call		= null;
 	var refresh_period	= opts['refresh_period'] || 1*1000;
 	var call_url		= opts['call_url'] || "http://localhost:4570/neoip_casti_ctrl_wpage_jsrest.js";
-	var casti_opts		= opts['casti_opts'];
+	var call_args		= opts['call_args'];
 	
 	var ctor	= function(){
 		// start the action
 		start();
 	}
 	
+	var request_stream	= function(){
+		// sanity check
+		assert.ok(rpc_call === null);
+		
+		var method_name	= "request_stream";
+		var rpc_call	= new neoip_rpc.call(call_url, method_name, call_args, function(cast_privhash){
+			console.log("succeed");	
+			console.log();
+		}, function(){
+			console.log("failed");
+		});		
+		
+	}
+	var release_stream	= function(){
+		// sanity check
+		assert.ok(rpc_call === null);
+		
+	}
+	
 	var refresh_cb	= function(){
 		// log to debug
 		console.log("refresh cb");
+		// sanity check
+		assert.ok(rpc_call === null);
+		assert.ok(refresh_timerid !== null);
 		// start the timeout
-		refresh_timerid	= setInterval(refresh_cb, refresh_period);
-	}
+		//refresh_timerid	= setInterval(refresh_cb, refresh_period);	}
 	
 	/**
 	 * Start the action
@@ -44,13 +66,19 @@ var casti_ctrl_t	= function(opts, user_cb){
 			clearTimeout( refresh_timerid );
 			refresh_timerid	= null;
 		}
+		// cancel rpc_call if needed
+		if( rpc_call !== null ){
+			rpc_call.stop();
+			rpc_call	= null;
+		}
 	}
 	
 	/**
 	 * return true if it is started, false otherwise
 	*/
 	self.is_started	= function(){
-		if( refresh_timerid === null )	return false;
+		if( refresh_timerid !== null )	return true;
+		if( rpc_call !== null )		return true;
 		return true;
 	}
 	
--- contrib/casti_stress_test/main.js
+++ contrib/casti_stress_test/main.js
@@ -7,29 +7,31 @@
  * - potentially a casto_bwlimiter_t
 */
 
-var casti_ctrl_t	= require('./casti_ctrl_t');
-
-var casti_ctrl	= casti_ctrl_t.create({
-	'casti_opts'	: {
-		'mdata_srv_uri'		: "http://localhost/~jerome/neoip_html/cgi-bin/cast_mdata_echo_server.fcgi",
-		'cast_name'		: "superstream",
-		'cast_privtext'		: "supersecret",
-		'scasti_uri'		: "http://127.0.0.1:8124",
-		'scasti_mod'		: "raw",
-		'http_peersrc_uri'	: "",
-		'web2srv_str'		: "dummyuserdata"
-	}
-}, function(error, cast_privhash){
-	console.log("error:"+error);
-	console.log("cast_privhash="+cast_privhash);
-});
+if(false){
+	var casti_ctrl_t	= require('./casti_ctrl_t');
+	
+	var casti_ctrl	= casti_ctrl_t.create({
+		'casti_opts'	: {
+			'mdata_srv_uri'		: "http://localhost/~jerome/neoip_html/cgi-bin/cast_mdata_echo_server.fcgi",
+			'cast_name'		: "superstream",
+			'cast_privtext'		: "supersecret",
+			'scasti_uri'		: "http://127.0.0.1:8124",
+			'scasti_mod'		: "raw",
+			'http_peersrc_uri'	: "",
+			'web2srv_str'		: "dummyuserdata"
+		}
+	}, function(error, cast_privhash){
+		console.log("error:"+error);
+		console.log("cast_privhash="+cast_privhash);
+	});
+}
 
 // url for casto "http://localhost:4560/a761ce3a/superstream"
 
-if( false ){
+if( true ){
 	var sys		= require('sys');
 	
-	var rpc_call	= require('../../lib/neoip_rpc').rpc_call;
+	var neoip_rpc	= require('../../lib/neoip_rpc');
 	
 	// start the probbing
 	var call_url	= "http://localhost:4550/neoip_oload_appdetect_jsrest.js";
@@ -45,7 +47,9 @@ if( false ){
 	var web2srv_str		= "dummyuserdata"
 	
 	if( process.argv[1] == __filename ){
-		rpc_call(call_url, "request_stream", mdata_srv_uri, cast_name, cast_privtext, scasti_uri, scasti_mod, http_peersrc_uri, web2srv_str, function(returned_val){
+		var method_name	= "request_stream";
+		var method_args	= [mdata_srv_uri, cast_name, cast_privtext, scasti_uri, scasti_mod, http_peersrc_uri, web2srv_str];
+		var rpc_call	= new neoip_rpc.call(call_url, method_name, method_args, function(returned_val){
 			console.log("succeed");	
 			console.log(sys.inspect(returned_val));
 		}, function(){
