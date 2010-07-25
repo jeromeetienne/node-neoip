990d4a8f42bfae8fd05b02d8a01b852ece984166
Jerome Etienne <jerome.etienne@gmail.com>
2010-07-25 14:16:46 +0200

    casti_stress_test: found my js style!!! 	renamed:    contrib/casti_stress_test/style.js -> contrib/casti_stress_test/casto_testclient_t.js

--- contrib/casti_stress_test/casto_testclient_t.js
+++ contrib/casti_stress_test/casto_testclient_t.js
@@ -2,19 +2,6 @@
 
 var http	= require('http');
 
-/**
- * opts:
- * - opts.stream_url	: url for the stream
- * - opts.notify_unit	: the unit to notify recved_data (default to 1024)
- * - opts.event_cb	: callback event_cb(event_type, event_data):
- *   - "cnx_begin"/null: server connected
- *   - "cnx_end"/null: server disconnected
- *   - "recved_size"/nunits: when data is received (nunit is the amount of data
- *     in 'unit'). it is notified when there is at least one unit to notified.
- *   - "idle_timeout"/null: when no data has been received for 
-*/
-var casto_testclient_t	= function(opts){
-
 /*
 - ctor
   - init var
@@ -28,27 +15,79 @@ var casto_testclient_t	= function(opts){
 - prototype approche is well known and well understood
 - does it support protected/private ?
   - yep but only at function level, not at the object level
-  - need a _myvar trick to show what is local or not
+  - need a _myvar trick to show what is local or not.
+    - CON: adversory only
+    - CON: polute the code
 - i need a clear and strict way to write it. no time to waste
 - prototype approche put a lot of this.* everywhere
+  - polute the code
 - ptototype approche limit the possibility of closure
 - prototype is not the common principle in js
 
 */
-	
-	
+
+/**
+ * opts:
+ * - opts.stream_url	: url for the stream
+ * - opts.notify_unit	: the unit to notify recved_data (default to 1024)
+ * - opts.event_cb	: callback event_cb(event_type, event_data):
+ *   - "cnx_begin"/null: server connected
+ *   - "cnx_end"/null: server disconnected
+ *   - "recved_size"/nunits: when data is received (nunit is the amount of data
+ *     in 'unit'). it is notified when there is at least one unit to notified.
+ *   - "idle_timeout"/null: when no data has been received for 
+*/
+var casto_testclient_t	= function(ctor_opts){
 	var self	= this;
 	// sanity check - all mandatory fields must be present
-	console.assert(opts.stream_url);
-	console.assert(opts.event_cb);
+	console.assert(ctor_opts.stream_url);
+	console.assert(ctor_opts.event_cb);
 	// alias some variables to ease readability
-	var stream_url	= opts.stream_url;
-	var event_cb	= opts.event_cb;
-	var notify_unit	= opts.notify_unit	|| 1024;
-	var verbose	= opts.verbose		|| 0;
+	var stream_url		= ctor_opts.stream_url;
+	var event_cb		= ctor_opts.event_cb		|| function(event_type, event_data){};
+	var notify_unit		= ctor_opts.notify_unit		|| 1024;
+	var verbose		= ctor_opts.verbose		|| 0;
+	var idle_timer_delay	= ctor_opts.idle_timer_delay	|| 1*1000;
+	
+	//////////////////////////////////////////////////////////////////////////
+	//		ctor/dtor						//
+	//////////////////////////////////////////////////////////////////////////
+	var ctor	= function(){
+		client_start();
+	}
+	var dtor	= function(){
+		idle_timer_stop();
+		client_stop();
+	}
+	
+	//////////////////////////////////////////////////////////////////////////
+	//		idle_timer						//
+	//////////////////////////////////////////////////////////////////////////
+	var idle_timer_id	= null;
+	var idle_timer_start	= function(delay){
+		// if not specified, set delay to default value
+		if( delay == undefined )	delay	= idle_timer_delay;
+		console.assert(idle_timer_id === null);
+		idle_timer_id	= setTimeout(idle_timer_cb, delay);
+	}
+	var idle_timer_stop	= function(){
+		if( idle_timer_id !== null )	clearTimeout(idle_timer_id);
+		idle_timer_id	= null;		
+	}
+	var idle_timer_refresh	= function(){
+		idle_timer_stop();
+		idle_timer_start();
+	}
+	var idle_timer_cb	= function(){
+		console.log("idle timer expired. next in "+idle_timer_delay+"-msec");
+		event_cb("idle_timeout", null);
+	}
 
-	var client_launch	= function(){
-		// init local variables
+	//////////////////////////////////////////////////////////////////////////
+	//		http client						//
+	//////////////////////////////////////////////////////////////////////////
+	var client_req		= null;
+	var client_start	= function(){
 		var recved_len	= 0;
 		var notified_len= 0;
 		// create the http client
@@ -58,19 +97,23 @@ var casto_testclient_t	= function(opts){
 		client.addListener("error"	, function(e){ event_cb("error", e.message);	});
 		client.addListener("timeout"	, function(e){ event_cb("error", e.message);	});
 		// create the request
-		var request	= client.request('GET', url.pathname, {'host': url.host});
-		request.addListener('response', function(response){
+		client_req	= client.request('GET', url.pathname, {'host': url.host});
+		client_req.on('response', function(client_res){
 			// log to debug
 			if(verbose > 0)	console.log("Connected to "+stream_url);
+			// start the idle_timer
+			idle_timer_start(0);
 			// Handle faillure at http level
-			if(response.statusCode != 200){
-				event_cb("error", "statusCode="+response.statusCode);
+			if(client_res.statusCode != 200){
+				event_cb("error", "statusCode="+client_res.statusCode);
 				return
 			}
 			// notify the caller
 			event_cb("cnx_begin", null);
-			//response.setEncoding('utf8');
-			response.addListener('data', function( chunk ){
+			//client_res.setEncoding('utf8');
+			client_res.on('data', function( chunk ){
+				// refresh idle_timer
+				idle_timer_refresh();
 				//console.log("chunk len="+chunk.length);
 				// update recved_len
 				recved_len	+= chunk.length;
@@ -82,24 +125,23 @@ var casto_testclient_t	= function(opts){
 				// update notified_len
 				notified_len		= recved_len;
 			});
-			response.addListener('end', function(){
+			client_res.on('end', function(){
 				// log the event
 				console.log("Connection ended");
 				// notify the caller
 				event_cb("cnx_end", null);
 			}); 
 		});
-		request.end();		
+		client_req.end();		
 	}
-	
-	var ctor	= function(){
-		client_launch();		
+	var client_stop		= function(){
+		client_req.connection.destroy();
 	}
-	
-	var dtor	= function(){
-		console.log("dtor")
-	}
-	
+
+	//////////////////////////////////////////////////////////////////////////
+	//									//
+	//////////////////////////////////////////////////////////////////////////
+	// call the contructor
 	ctor();
 	// return the public properties
 	return {
@@ -107,86 +149,27 @@ var casto_testclient_t	= function(opts){
 	}
 }
 
-casto_client_t = function(opts)
-{
-	var self	= this;
-	// sanity check - all mandatory fields must be present
-	console.assert(opts.stream_url);
-	console.assert(opts.event_cb);
-	// alias some variables to ease readability
-	self.stream_url	= opts.stream_url;
-	self.event_cb	= opts.event_cb;
-	self.notify_unit= opts.notify_unit	|| 1024;
-	self.verbose	= opts.verbose		|| 0;
-
-	this._idle_timeout_start();
-}
-
 /**
- * Destructor
+ * Class method to create an object
+ * - thus avoid new operator
 */
-casto_client_t.prototype.dtor	= function()
-{
-	this._idle_timout_stop();
-	this._client_stop();
-}
-
-//////////////////////////////////////////////////////////////////////////////////
-//		idle_timeout							//
-//////////////////////////////////////////////////////////////////////////////////
-casto_client_t.prototype._idle_timeout_start	= function()
-{
-	console.assert(this.idle_timeout_id === null || this.idle_timeout_id === undefined);
-	this.idle_timeout_id	= setTimeout(this._idle_timeout_cb, 1*1000);
-	console.log("ddd");
-}
-
-casto_client_t.prototype._idle_timeout_stop	= function()
-{
-	if( this.idle_timeout_id !== null )	clearTimeout(this.idle_timeout_cb);
-	this.idle_timeout_id	= null;
-	this._idle_timeout_start();
-}
-
-casto_client_t.prototype._idle_timeout_cb	= function()
-{
-	console.log("idle timeout expired. next in "+this._idle_timeout_delay);
-}
-
-//////////////////////////////////////////////////////////////////////////////////
-//		client								//
-//////////////////////////////////////////////////////////////////////////////////
-casto_client_t.prototype._client_start	= function()
-{
-}
-
-casto_client_t.prototype._client_stop	= function()
-{
+casto_testclient_t.create	= function(ctor_opts){
+	return new casto_testclient_t(ctor_opts);
 }
 
 //////////////////////////////////////////////////////////////////////////////////
 //	main programm								//
 //////////////////////////////////////////////////////////////////////////////////
 if( process.argv[1] == __filename ){
+	// TODO dunno why but the actual url is never
+	stream_url	= "http://127.0.0.1:8124/";
+	console.log("stream_url="+stream_url);
 
-	// build stream_url for neoip-casto
-	var stream_url	= require('./casto_url').create({
-		"base_url"	: "http://localhost:4560",
-		"cast_privhash"	: "a761ce3a",
-		"cast_name"	: "superstream"
-	});
-
-// TODO dunno why but the actual url is never
-stream_url	= "http://127.0.0.1:8124/";
-console.log("stream_url="+stream_url);
-
-if(false){
-	var client	= new casto_testclient_t({
+	var client	= casto_testclient_t.create({
 		stream_url	: stream_url,
 		verbose		: 1,
 		event_cb	: function(event_type, event_data){
-			//console.log("event_type="+event_type);
-			//console.log("event_data="+event_data);
+			//console.log("event_type="+event_type+" event_data="+event_data);
 			if( event_type == "recved_size" ){
 				var nb_unit	= event_data;
 				for(var i = 0; i < nb_unit; i++){
@@ -195,22 +178,4 @@ if(false){
 			}
 		}
 	});
-}
-
-if(true){
-	var client	= new casto_client_t({
-		stream_url	: stream_url,
-		verbose		: 1,
-		event_cb	: function(event_type, event_data){
-			//console.log("event_type="+event_type);
-			//console.log("event_data="+event_data);
-			if( event_type == "recved_size" ){
-				var nb_unit	= event_data;
-				for(var i = 0; i < nb_unit; i++){
-					require("sys").print('.');
-				}
-			}
-		}
-	});	
-}
 }
