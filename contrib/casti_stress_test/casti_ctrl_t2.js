58fa766a992f1a53ee01daa9b1198718c7abefbf
Jerome Etienne <jerome.etienne@gmail.com>
2010-07-27 11:41:38 +0200

    casti_ctrl_t2: cmdline support 	modified:   casti_ctrl_t2.js

--- contrib/casti_stress_test/casti_ctrl_t2.js
+++ contrib/casti_stress_test/casti_ctrl_t2.js
@@ -150,18 +150,69 @@ exports.create	= casti_ctrl_t.create;
 //	main programm								//
 //////////////////////////////////////////////////////////////////////////////////
 if( process.argv[1] == __filename ){
-// TODO make this a good cmdline tool for testing
-// thus making it a good testing tool
-// --no_gracefull_shutdown
-// --casti_opts key value
-// --call_url url
-// --req_timer_delay delay_msec
-// --verbose
-// if call_url unspecified, try to autodetect casti on localhost
+// ************************************
+// - in neoip-casti
+//   - idle timeout (given by casti cmd with default. with a max in .conf)
+//   - faster publishing
+
+
+	//////////////////////////////////////////////////////////////////////////////////
+	//	parse cmdline								//
+	//////////////////////////////////////////////////////////////////////////////////
+	// cmdline_opts default
+	cmdline_opts	= {
+		casti_opts		: {},
+		call_url		: null,
+		req_timer_delay		: null,
+		verbose			: 0,
+		no_gracefull_shutdown	: false
+	};
+	var disp_usage	= function(prefix){
+		if(prefix)	console.log(prefix + "\n");
+		console.log("usage: casti_ctrl -o key val -u url [-d msec] [-n] [-v [-v]]");
+		console.log("");
+		console.log("Control neoip-casti");
+		console.log("");
+		console.log("-o|--casti_opts key val\t\tSet the key/val options for casti call.");
+		console.log("-u|--call_url url\t\tSet the call_url for the rpc");
+		console.log("-d|--req_timer_delay sec\tSet the delay between 2 request_stream calls.");
+		console.log("-n|--no_gracefull_shutdown\tDont explicitly release the stream on SIGINT");
+		console.log("-v|--verbose\t\t\tIncrease the verbose level (for debug).");
+		console.log("-h|--help\t\t\tDisplay the inline help.");
+	}
+	var optind	= 2;
+	for(;optind < process.argv.length; optind++){
+		var key	= process.argv[optind];
+		var val	= process.argv[optind+1];
+		//console.log("key="+key+" val="+val);
+		if( key == '-o' || key == "--casti_opts" ){
+			cmdline_opts.casti_opts[val]	= process.argv[optind+2];
+			optind		+= 2;
+		}else if( key == '-u' || key == "--call_url" ){
+			// TODO if call_url unspecified, try to autodetect casti on localhost			
+			cmdline_opts.call_url	= val;
+			optind		+= 1;
+		}else if( key == '-d' ||key == "--req_timer_delay" ){
+			cmdline_opts.req_timer_delay	= parseFloat(val)*1000;
+			optind		+= 1;
+		}else if( key == '-n' || key == "--no_gracefull_shutdown" ){
+			cmdline_opts.no_gracefull_shutdown	= true;
+		}else if( key == '-v' || key == "--verbose" ){
+			cmdline_opts.verbose	+= 1;
+		}else if( key == "-h" || key == "--help" ){
+			disp_usage();
+			process.exit(0);
+		}else{
+			// if the option doesnt exist, consider it is the first non-option parameters
+			break;
+		}
+	}
 	
-	var casti_ctrl	= casti_ctrl_t.create({
-		call_url	: "http://localhost:4570/neoip_casti_ctrl_wpage_jsrest.js",
-		casti_opts	: {
+	if(false){
+		// example of cmdline
+		// node casti_ctrl_t2.js --call_url http://localhost:4570/neoip_casti_ctrl_wpage_jsrest.js -o mdata_srv_uri http://localhost/~jerome/neoip_html/cgi-bin/cast_mdata_echo_server.fcgi -o cast_name superstream -o cast_privtext supersecret -o scasti_uri http://127.0.0.1:8124 -o scasti_mod raw -o http_peersrc_uri '' -o web2srv_str 'dummyweb2serv_str' -v 
+		cmdline_opts.call_url	= "http://localhost:4570/neoip_casti_ctrl_wpage_jsrest.js";
+		cmdline_opts.casti_opts	= {
 			mdata_srv_uri	: "http://localhost/~jerome/neoip_html/cgi-bin/cast_mdata_echo_server.fcgi",
 			cast_name	: "superstream",
 			cast_privtext	: "supersecret",
@@ -169,15 +220,31 @@ if( process.argv[1] == __filename ){
 			scasti_mod	: "raw",
 			http_peersrc_uri: "",
 			web2srv_str	: "dummyuserdata"
-		},
+		};
+	}
+	
+	
+
+	// build casti_ctrl ctor_opts
+	var ctor_opts	= {
+		call_url	: cmdline_opts.call_url,
+		casti_opts	: cmdline_opts.casti_opts,
 		event_cb	: function(event_type, event_data){
 			console.log("event_cb: type="+event_type+" data="+require('sys').inspect(event_data));
 		}
-	});
-	// trap SIGINT - first = release(), second normal behavior
-	process.addListener('SIGINT', function(){
-		console.log("Received sigint, start releasing the stream");
-		casti_ctrl.release();
-		process.removeListener('SIGINT', arguments.callee);
-	});
+	};
+	ctor_opts.req_timer_delay	= cmdline_opts.req_timer_delay	|| null;
+	ctor_opts.verbose		= cmdline_opts.verbose		|| 0;
+	// init casti_ctrl
+	var casti_ctrl	= casti_ctrl_t.create(ctor_opts);
+
+	// init gracefull_shutdown if not specified otherwise in cmdline
+	if( cmdline_opts.no_gracefull_shutdown === false ){
+		// trap SIGINT - first = release(), second normal behavior
+		process.addListener('SIGINT', function(){
+			console.log("Received sigint, start releasing the stream");
+			casti_ctrl.release();
+			process.removeListener('SIGINT', arguments.callee);
+		});
+	}
 }
