2f14eba375d397a1be0ff1fa9b85dd5cdd9c1934
Jerome Etienne <jerome.etienne@gmail.com>
2010-08-03 15:42:32 +0200

    contrib/www: better version of neoip_rpc_web 	deleted:    contrib/www/index.html 	new file:   contrib/www/index2.html 	renamed:    contrib/www/probe_app.js -> contrib/www/neoip_jsonp_call.js 	new file:   contrib/www/neoip_rpc_web.js

--- contrib/www/neoip_jsonp_call.js
+++ contrib/www/neoip_jsonp_call.js
@@ -1,13 +1,29 @@
-var jsonp_call_counter	= 0;
+// declare neoip namespace
+var neoip	= neoip || {};
+
+/**
+ * a serial to uniquely define a given call
+*/
+neoip.jsonp_call_counter	= 0;
+
 /**
  * do a jsonp call
  * - NOTE: needed because jquery getJSON is not able to handle failure_cb
- * - jquery getJSON is 
 */
-var jsonp_call	= function(url, succeed_cb, failure_cb)
+neoip.jsonp_call	= function(ctor_opts)
 {
+	//////////////////////////////////////////////////////////////////////////
+	//		class variables						//
+	//////////////////////////////////////////////////////////////////////////
+	// alias 'this' for this object, to self
+	var self	= this;
+	// copy ctor_opts + set default values if needed
+	var url		= ctor_opts.url		|| console.assert(ctor_opts.url);
+	var success_cb	= ctor_opts.success_cb	|| function(data){};
+	var failure_cb	= ctor_opts.failure_cb	|| function(error){};
+	
 	// determine the function name
-	var fct_name	= "jsonp_call_cb_"+jsonp_call_counter++;
+	var fct_name	= "jsonp_call_cb_"+neoip.jsonp_call_counter++;
 	// sanity check - callback=? MUST be present in the url
 	console.assert( /callback=\?(&|$)/.test(url) );
 	// replace callback=? by fct_name
@@ -32,7 +48,7 @@ var jsonp_call	= function(url, succeed_cb, failure_cb)
 	// declare the jsonp callback in window[]
 	window[fct_name]	= function(data){
 		cleanup()
-		succeed_cb(data);
+		success_cb(data);
 	};
 
 	// create the script element
@@ -42,26 +58,13 @@ var jsonp_call	= function(url, succeed_cb, failure_cb)
 		failure_cb("Network error");		
 	}
 	// append the script element to HEAD
-	root_elem.appendChild(script_elem);
+	root_elem.appendChild(script_elem);	
+
+	//////////////////////////////////////////////////////////////////////////
+	//		run initialisation					//
+	//////////////////////////////////////////////////////////////////////////
+	// return the public properties
+	return {
+		destroy	: cleanup
+	}
 }
-
-
-var probe_app_browser = function(apps_suffix, host, port, method_name, success_cb, failure_cb){
-	var path	= "neoip_"+apps_suffix+"_appdetect_jsrest.js?callback=?&method_name="+method_name;
-	var url		= "http://"+host+":"+port+"/"+path;
-	jsonp_call(url, function(data){
-		var returned_val	= data['returned_val']
-		success_cb(returned_val);
-	}, function(reason){
-		failure_cb(reason);
-	});
-}
-
-
-/** @define {boolean} */
-var IN_NODEJS = false;
-
-var probe_app	= null;
-if( IN_NODEJS )	probe_app	= function(){};
-else		probe_app	= probe_app_browser;
-window['probe_app']	= probe_app;
---
+++ contrib/www/neoip_rpc_web.js
@@ -0,0 +1,80 @@
+// declare neoip namespace
+var neoip	= neoip || {};
+
+/**
+ * do a rpc call to a neoip application 
+*/
+neoip.rpc_call = function(ctor_opts){
+	//////////////////////////////////////////////////////////////////////////
+	//		class variables						//
+	//////////////////////////////////////////////////////////////////////////
+	// copy ctor_opts + set default values if needed
+	var call_url	= ctor_opts.call_url	|| console.assert(ctor_opts.call_url);
+	var method_name	= ctor_opts.method_name	|| console.assert(ctor_opts.method_name);
+	var method_args	= ctor_opts.method_args	|| [];
+	var success_cb	= ctor_opts.success_cb	|| function(returned_val){};
+	var failure_cb	= ctor_opts.failure_cb	|| function(error){};
+	var verbose	= ctor_opts.verbose	|| 0;
+
+	//////////////////////////////////////////////////////////////////////////
+	//		ctor/dtor						//
+	//////////////////////////////////////////////////////////////////////////
+	var ctor	= function(){
+		client_start();
+	}
+	var dtor	= function(){
+		//console.log("Stop neoip rpc call");
+		client_stop();
+	}
+
+	//////////////////////////////////////////////////////////////////////////
+	//		http client						//
+	//////////////////////////////////////////////////////////////////////////
+	var client_call		= null;
+	var client_start	= function(){
+		// sanity check
+		console.assert( client_call == null );
+		// build url_path
+		var url	= call_url + "?callback=?&method_name=" + method_name;
+		for(var i = 0; i < method_args.length; i++){
+			url	+= "&arg"+i+"=" + escape(method_args[i]);
+		}
+		console.log("url="+url);
+		// create the jsonp_call
+		client_call	= new neoip.jsonp_call({
+			url		: url,
+			success_cb	: function(data){
+				console.dir(data);
+				// handle faillure/success_cb at the call level
+				if( data['fault'] ){
+					failure_cb(data['fault']);	// TODO what about the error itself	
+				}else{
+					var returned_val= data['returned_val'];
+					success_cb(returned_val);
+				}				
+			},
+			failure_cb	: function(error){
+				// log to debug
+				console.log("failed due to "+error);
+				// notify the caller
+				failure_cb(error);
+			}
+		})
+
+	}	
+	var client_stop		= function(){
+		// destroy client_call if needed
+		if( client_call )	client_call.destroy();
+		client_call	= null;
+	}
+	
+	//////////////////////////////////////////////////////////////////////////
+	//		run initialisation					//
+	//////////////////////////////////////////////////////////////////////////
+	// call the contructor
+	ctor();
+	// return the public properties
+	return {
+		destroy	: dtor
+	}
+};
