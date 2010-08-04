2181c5f5c407736550e2cb98805cd68820ff88e9
Jerome Etienne <jerome.etienne@gmail.com>
2010-08-04 09:30:53 +0200

    move naw experimentation into http://github.com/jeromeetienne/InBrowserCommonJS 	deleted:    contrib/naw/naw.js 	deleted:    contrib/naw/naw_example_module.js 	deleted:    contrib/naw/naw_example_test.js

--- contrib/naw/naw_example_module.js
+++
@@ -1,8 +0,0 @@
-var naw = require("./naw");
-
-naw({sys: "sys"}, "example_module", exports, function(exports, require){
-	require('sys').puts("testPPP");
-	sys.puts("pp ok SYS passed");
-	//console.dir(exports);
-	exports.test	= function(){ console.log("output of exported function") }
-})
--- contrib/naw/naw_example_test.js
+++
@@ -1,7 +0,0 @@
-
-var naw = require("./naw");
-
-naw({"exmod"	: "./naw_example_module"}, "example_test", exports, function(exports, require){
-	console.log("inside naw example test");
-	exmod.test();
-});
