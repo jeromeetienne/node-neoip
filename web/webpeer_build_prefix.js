884e7c3de59b7421d78d98e38c7e2850b0e40eed
Jerome Etienne <jerome.etienne@gmail.com>
2010-08-14 00:35:52 +0200

    web build: put library in a closure to exports only public symbols 	modified:   Makefile 	modified:   index.html 	new file:   webpeer_build_prefix.js 	new file:   webpeer_build_suffix.js

---
+++ web/webpeer_build_prefix.js
@@ -0,0 +1,9 @@
+/**
+ * This file will be prepended to webpeer.js during the builder
+*/
+
+/**
+ * Library closure
+ * * GLOBAL is the external global
+*/
+(function(GLOBAL){
---
+++ web/webpeer_build_suffix.js
@@ -0,0 +1,9 @@
+/**
+ * This file will be happened to webpeer.js during the builder
+*/
+
+// exports the public symbols
+GLOBAL.webpeer	= require('./webpeer');
+
+// end of the global closure
+})(window);
