aeabc6bb9b0e1e066b1f4695fad0745d378dab8f
Jerome Etienne <jerome.etienne@gmail.com>
2010-08-14 01:47:05 +0200

    web_build: quite convincing 	modified:   web_build/Makefile 	modified:   web_build/build_prefix.js 	modified:   web_build/webpeer-0.7.0-min.js 	modified:   web_build/webpeer-0.7.0.js

--- web_build/build_prefix.js
+++ web_build/build_prefix.js
@@ -7,3 +7,16 @@
  * * GLOBAL is the external global
 */
 (function(GLOBAL){
+
+
+// firebugx - 
+// see http://code.google.com/p/fbug/source/browse/branches/firebug1.2/lite/firebugx.js
+if (!window.console)
+{
+    var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml",
+    "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
+
+    window.console = {};
+    for (var i = 0; i < names.length; ++i)
+        window.console[names[i]] = function() {}
+}
--- web_build/webpeer-0.7.0.js
+++ web_build/webpeer-0.7.0.js
@@ -8,6 +8,19 @@
  * * GLOBAL is the external global
 */
 (function(GLOBAL){
+
+
+// firebugx - 
+// see http://code.google.com/p/fbug/source/browse/branches/firebug1.2/lite/firebugx.js
+if (!window.console)
+{
+    var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml",
+    "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
+
+    window.console = {};
+    for (var i = 0; i < names.length; ++i)
+        window.console[names[i]] = function() {}
+}
 // Brequire - CommonJS support for the browser
 function require(path) {
   var module = require.modules[path]
@@ -1156,61 +1169,6 @@ var create = function(opts){
 // export it via commonjs
 exports.create	= create;
 
-//////////////////////////////////////////////////////////////////////////////////
-//	main programm								//
-//////////////////////////////////////////////////////////////////////////////////
-if( process.argv[1] == __filename ){
-	opts	= {
-		base_url	: null,
-		cast_privhash	: null,
-		cast_name	: null,
-		mdata_srv_uri	: null
-	};
-
-	//////////////////////////////////////////////////////////////////////////////////
-	//	parse cmdline								//
-	//////////////////////////////////////////////////////////////////////////////////
-	var disp_usage	= function(prefix){
-		if(prefix)	console.log(prefix + "\n");
-		console.log("usage: neoip-url-stream [-s url] base_url cast_privhash cast_name");
-		console.log("");
-		console.log("Build an url for neoip-casto");
-		console.log("");
-		console.log("-s|--mdata_srv_uri\n\t\tSet url for the mdata_srv.");
-		console.log("-h|--help\tDisplay the inline help.");
-	}
-	var optind	= 2;
-	for(;optind < process.argv.length; optind++){
-		var key	= process.argv[optind];
-		var val	= process.argv[optind+1];
-		//console.log("key="+key+" val="+val);
-		if( key == "-s" || key == "--mdata_srv_uri" ){
-			opts.mdata_srv_uri	= val;
-			optind		+= 1;			
-		}else if( key == "-h" || key == "--help" ){
-			disp_usage();
-			process.exit(0);
-		}else{
-			// if the option doesnt exist, consider it is the first non-option parameters
-			break;
-		}
-	}
-	// check there are at least 3 parameters remaining in the cmdline
-	if( process.argv.length - optind < 3 ){
-		disp_usage("missing parameters to buidl the url");
-		process.exit(0);		
-	}
-	
-	// get required options from the rest of the cmdline
-	opts.base_url		= process.argv[optind++];
-	opts.cast_privhash	= process.argv[optind++];
-	opts.cast_name		= process.argv[optind++];
-	
-	// create the url
-	var url	= create(opts);
-	// output the url on stdout
-	console.log(url);	
-}
 
 // end module
 });require.module('url_builder_oload_t', function(exports, require) {
